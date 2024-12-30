121b488cc77f395c88c39935880d4adfe1f54281
Pär Hedström <h24parhe@du.se>
2024-12-30 11:37:26 +0100

    .

--- Labb4/obj/Release/net9.0/Labb4.AssemblyInfo.cs
+++ Labb4/obj/Release/net9.0/Labb4.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb4")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d8730166fbf02b75c52369a4ac802add51ac5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb4")]
